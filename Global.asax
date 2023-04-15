f1b4aa8fce268421bd13ad5cefd079c70695261a
Nguyen An <nguyenngocanit002@gmail.com>
2023-04-15 23:57:30 +0700

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Food_Sharing_Food.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Food_Sharing_Food.Startup))]
+namespace Food_Sharing_Food
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
